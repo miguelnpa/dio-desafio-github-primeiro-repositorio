3edddde037f59c3b7834f0c3811f72e08a55a5bf
Miguel Nunes <miguel.npa@gmail.com>
2022-01-03 13:48:02 -0300

    atualização

--- Programando_Orientacao_Objetos/Abstracao/ExemploPOO/Program.cs
+++ Programando_Orientacao_Objetos/Abstracao/ExemploPOO/Program.cs
@@ -1,15 +1,76 @@
 ﻿using System;
+using System.IO;
 using ExemploPOO.Models;
+using ExemploPOO.Interfaces;
+using ExemploPOO.Helper;
+using System.Collections.Generic;
+
 namespace ExemploPOO
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var caminho = "C:\\TrabalhandoComArquivos";
+            var caminhoPathCombine = Path.Combine(caminho, "Pasta Teste 1");
+            var caminhoArquivo = Path.Combine(caminho, "arquivo-teste-stream.txt");
+            var caminhoArquivoTeste = Path.Combine(caminho, "arquivo-teste.txt");
+            var caminhoArquivoTesteCopia = Path.Combine(caminho, "arquivo-teste-bkp.txt");
+            var novocaminhoArquivo = Path.Combine(caminho,"Pasta Teste 2","arquivo-teste-stream.txt");
 
-            Calculadora c = new Calculadora();
-            System.Console.WriteLine($"Resultado da primeira soma: {c.Somar(10,20)}");
-            System.Console.WriteLine($"Resultado da segunda soma: {c.Somar(10,20,30)}");
+            FileHelper helper = new FileHelper();
+
+            var listaString = new List<string> {"linha 1","linha 2", "linha 3"};
+            var listaStringContinuacao = new List<string> {"linha 4","linha 5", "linha 6"};
+
+            helper.DeletarArquivo(caminhoArquivoTesteCopia);
+
+            // helper.CopiarArquivo(caminhoArquivoTeste, caminhoArquivoTesteCopia, false);
+
+            // helper.MoverArquivo(caminhoArquivo, novocaminhoArquivo,false);
+
+            // helper.LerArquivoStream(caminhoArquivo);
+
+            // helper.CriarArquivoTextoStream(caminhoArquivo, listaString);
+            // helper.AdicionarTextoStream(caminhoArquivo,listaStringContinuacao);
+    
+            // helper.CriarArquivoTextoStream(caminhoArquivo, listaString);
+
+            // helper.CriarArquivoTexto(caminhoArquivo, "Olá! Teste de escrita de arquivo");
+
+            // var caminho = "C:\\TrabalhandoComArquivos";
+            // var caminhoPathCombine = Path.Combine(caminho, "Pasta Teste 1");
+            // FileHelper helper = new FileHelper();
+            // helper.ApagarDiretorio(caminhoPathCombine, true);
+
+            // var caminho = "C:\\TrabalhandoComArquivos";
+            // var caminhoPathCombine = Path.Combine(caminho, "Pasta teste 3", "subPastaTeste3");
+            // FileHelper helper = new FileHelper();
+            // helper.CriarDiretorio(Path.Combine(caminhoPathCombine));
+            // System.Console.WriteLine("Criando diretório: " + caminhoPathCombine);
+
+            // var caminho = "C:\\TrabalhandoComArquivos";
+            // FileHelper helper = new FileHelper();
+            // helper.ListarArquivosDiretorios(caminho);
+
+
+            // var caminho = "C:\\DIO\\dio-repositorio\\Programando_Orientacao_Objetos\\Abstracao\\ExemploPOO";
+            // FileHelper helper = new FileHelper();
+            // helper.ListarDiretorios(caminho);
+
+            // ICalculadora calc = new Calculadora();
+            // System.Console.WriteLine(calc.Somar(10,20));
+
+            // Computador comp = new Computador();
+            // System.Console.WriteLine(comp.ToString());
+
+            // Corrente c = new Corrente();
+            // c.Creditar(100);
+            // c.ExibirSaldo();
+
+            // Calculadora c = new Calculadora();
+            // System.Console.WriteLine($"Resultado da primeira soma: {c.Somar(10,20)}");
+            // System.Console.WriteLine($"Resultado da segunda soma: {c.Somar(10,20,30)}");
 
             // Aluno p1 = new Aluno();
             // p1.Nome = "Bob";
--- Programando_Orientacao_Objetos/README.md
+++ Programando_Orientacao_Objetos/README.md
@@ -47,7 +47,43 @@ Ex.: public virtual void Apresentar() {}
 
 ## Classes abstratas
 
+Uma classe abstrata tem como objetivo ser exclusivamente um modelo para ser herdade, portanto não pode ser instanciada.
+
+Você pode implementar métodos ou dexa-los a cargo de quem herdar
+
+## Classes seladas
+
+Uma classe selada tem como objetivo de impedir que outras classes façam uma herança dela, ou seja, nenhuma classe pode ser sua derivada. 
+
+Também existem métodos e propriedade seladas.
+
+Na aula, é usado o exemplo da classe Pessoa => professor => e a classe Diretor tentando ser filha da classe professor, mas a classe professor é selada e não permite isso.
+
+## Classes Object
+
+A classe Sustem.Object é a mãe de todas as classes na hierarquia do .NET
+
+Todas as classes derivam, direta ou indiretamente da classe Object, e ela tem como objetivo prover serviços de baixo nível para suas classes filhas.
+
 ## Interfaces
 
+Uma interface é um contrato que pode ser implementado por uma classe 
+
+É como se fosse uma classe abstrata, podendo definir métodos abstratos para serem implementados.
+
+Assim como uma classe abstrata, uma interface não pode ser instanciada.
+
+(A interface veio para fazer algo como herança múltipla.)
+
+Para a classe que implementa uma interface, ela tem que implementar todos os métodos da interface ( caso a interface não possua implementação padrão.)
+
 ## Manipulando arquivos
+
+O C# apresenta alguams classes estáticas que facilitam o trabalho com arquivos, dentre elas: 
+
+File - Serve para manipular arquivos (criar, ler,mover, apagar)
+Directory - listar diretórios, criar diretórios, Obter informação de diretórios
+Path - Serve para organizar os caminhos de mateira lógica para trabalhar com File e Directory.
+
+Path.combine são um conjunto de objeto/método que ajudam na criação de diretórios.
 
